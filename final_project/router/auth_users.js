a48300139b2b852bdc0bb5fc585e398c2959a7c2
shuklashivam-devstree <shivam.s@devstree.in>
2026-04-03 12:16:44 +0530

    added delete api

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -72,6 +72,29 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 });
 
+// delete the review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const username =
+    req.session &&
+    req.session.authorization &&
+    req.session.authorization.username;
+  const isbn = req.params.isbn;
+
+  if (!username) {
+    return res.status(403).json({ message: "User not authorized" });
+  }
+  if (!isbn || !books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  delete books[isbn].reviews[username];
+
+  return res.status(200).json({
+    message: "Review deleted successfully",
+    reviews: books[isbn].reviews,
+  });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
